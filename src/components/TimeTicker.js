32f8298eafc10821f4c1b0ddb97b8933d9d996e7
suglobov <suglobov@mail.ru>
2020-10-09 14:33:22 +0700

    📦refactor: Упростил тикер времени.

--- src/components/TimeTicker.js
+++ src/components/TimeTicker.js
@@ -1,39 +1,24 @@
 class TimeTicker {
     constructor({
-        callbackTiс = () => { },
-        callbackSeconds = () => { },
-        callback100ms = () => { },
+        delayMs = 10,
+        callback = () => { },
     }) {
-        this.callbackTiс = callbackTiс;
-
-        this.timer100;
-        this.count100ms = 0;
-        this.callbackSeconds = callbackSeconds;
-        this.callback100ms = callback100ms;
+        this.interval;
+        this.delayMs = delayMs;
+        this.callback = callback;
         this.isTimerRunning = false;
-
-        setInterval(() => {
-            if (this.isTimerRunning) {
-                this.callbackTiс();
-            }
-        }, 1000);
     }
 
     startTimer() {
         this.isTimerRunning = true;
-        this.timer100 = setInterval(() => {
-            this.count100ms += 1;
-            this.callback100ms();
-            if (this.count100ms >= 10) {
-                this.callbackSeconds();
-                this.count100ms = 0;
-            }
-        }, 100);
+        this.interval = setInterval(this.callback, this.delayMs);
+        return this;
     }
 
     stopTimer() {
         this.isTimerRunning = false;
-        clearInterval(this.timer100);
+        clearInterval(this.interval);
+        return this;
     }
 }
 
