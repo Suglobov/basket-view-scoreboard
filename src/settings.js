1e148cc43d496deb90e09800b51610652a1d93da
suglobov <suglobov@mail.ru>
2020-10-11 23:06:30 +0700

    📦refactor: Чуть более объектно ориентированное время. Пока что счетчик 24 не задействован.

--- src/settings.js
+++ src/settings.js
@@ -1,11 +1,9 @@
 import './js/common.js';
 import WebSocketConnect from './components/WebSocketConnect.js';
 import TimeTicker from './components/TimeTicker.js';
-// import TimeObject from './components/TimeObject.js';
 
 
 const listenersDelay = 500;
-
 const HOST = location.origin.replace(/^http/, 'ws');
 const webSocket = new WebSocketConnect({
     url: HOST,
@@ -40,8 +38,16 @@ const dom = {
     counter24: document.querySelector('[data-name="counter24"]'),
     minutes: document.querySelector('[data-name="minutes"]'),
     seconds: document.querySelector('[data-name="seconds"]'),
-    ms100: document.querySelector('[data-name="ms100"]'),
+    tenthsOfSecond: document.querySelector('[data-name="tenthsOfSecond"]'),
     mirror: document.querySelector('[data-name="mirror"]'),
+};
+
+const debounce = (func, delay) => {
+    let timeout;
+    return function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, arguments), delay);
+    };
 };
 
 const stopTimer = function () {
@@ -53,169 +59,94 @@ const startTimer = function () {
     dom.clockControl.classList.add('time-running');
 };
 
-// const timeToDom = ({ timeObj, dom }) => {
-//     dom.minutes.value = timeObj.minutes;
-//     dom.seconds.value = timeObj.seconds;
-//     dom.ms100.value = timeObj.ms100;
-// };
-
-// const timeObjectMinusSecond = ({ timeObject, stopTimer }) => {
-//     const fullSeconds = timeObject.getFullSeconds();
-//     if (fullSeconds <= 0) {
-//         stopTimer();
-//         return;
-//     }
-//     timeObject.setFullSeconds(fullSeconds - 1);
-// };
-
-// const saveTimeFromDomToObject = ({ timeObject, dom }) => {
-//     const minutes = Number(dom.minutes.value);
-//     const seconds = Number(dom.seconds.value);
-//     timeObject.setTime(minutes, seconds);
-// };
-// const saveTimeFromObjectToDom = ({ timeObject, dom }) => {
-//     dom.minutes.value = timeObject.getMinutes();
-//     dom.seconds.value = timeObject.getSeconds();
-// };
-
-// const saveCounter24 = ({ newValue, dom, timeObject }) => {
-//     if (newValue < 0) {
-//         return;
-//     }
-//     const fullSeconds = timeObject.getFullSeconds();
-//     dom.counter24.value = (fullSeconds < newValue) ? fullSeconds : newValue;
-// };
-
-// const sendTime = function ({ timeObject, webSocket }) {
-//     const minutes = timeObject.getMinutes();
-//     const seconds = timeObject.getSeconds();
-//     webSocket.sendJSON({
-//         name: 'time', value: { minutes, seconds, },
-//     });
-// };
-
-const debounce = (func, delay) => {
-    let timeout;
-    return function () {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, arguments), delay);
-    };
-};
-
-// сonst timeObj = {
-//     ms100InMinutes: 600,
-//     ms100InSeconds: 10,
-//     ms100: 0,
-//     seconds: 5,
-//     minutes: 0,
-//     minus100ms({ callbackTimeIsEnd = () => { }, callbackMinusSuccess = () => { } }) {
-//         const currentFull100ms = this.minutes * this.ms100InMinutes
-//             + this.seconds * this.ms100InSeconds
-//             + this.ms100;
-//         if (currentFull100ms <= 0) {
-//             callbackTimeIsEnd();
-//             return;
-//         }
-//         const newFull100ms = currentFull100ms - 1;
-
-//         this.counter24 = this.counter24 - 1;
-//         const fullSeconds = newFull100ms * this.ms100InSeconds;
-//         if (fullSeconds < this.counter24) {
-//             this.counter24 = fullSeconds;
-//         }
-//         this.setTimeFull100Ms(newFull100ms);
-//         callbackMinusSuccess();
-//     },
-//     setTimeFull100Ms(full100ms) {
-//         this.minutes = Math.floor(full100ms / this.ms100InMinutes);
-//         const remainOfMinutes = full100ms - this.minutes * this.ms100InMinutes;
-//         this.seconds = Math.floor(remainOfMinutes / this.ms100InSeconds);
-//         const remainOfSeconds = remainOfMinutes - this.seconds * this.ms100InSeconds;
-//         this.ms100 = remainOfSeconds;
-//     },
-// };
-const MS100_IN_MINUTES = 600;
-const MS100_IN_SECONDS = 10;
-
-let fullMs100 = 50;
 let counter24 = 24;
 
-const getTimeObject = (fullMs100) => {
-    const minutes = Math.floor(fullMs100 / MS100_IN_MINUTES);
-    const remainOfMinutes = fullMs100 - minutes * MS100_IN_MINUTES;
-    const seconds = Math.floor(remainOfMinutes / MS100_IN_SECONDS);
-    const remainOfSeconds = remainOfMinutes - seconds * MS100_IN_SECONDS;
-    const ms100 = remainOfSeconds;
-    return { minutes, seconds, ms100, };
+const timeObject = {
+    tenthsOfSecond: {
+        value: 0,
+        min: 0,
+        max: 9,
+        higher: 'seconds',
+    },
+    seconds: {
+        value: 0,
+        min: 0,
+        max: 59,
+        higher: 'minutes',
+    },
+    minutes: {
+        value: 1,
+        min: 0,
+        max: 10,
+    },
 };
 
-const getCorrectCounter24 = (newValue, fullMs100) => {
-    const fullSeconds = Math.floor(fullMs100 / MS100_IN_SECONDS);
-    return (newValue > fullSeconds) ? fullSeconds : newValue;
-};
-
-const getFullMs100FromTime = ({ minutes, seconds, ms100 }) => {
-    let acc = 0;
-    if (minutes !== undefined) {
-        acc += minutes * MS100_IN_MINUTES;
+const minusTime = ({
+    timeObject,
+    part,
+    cbSuccess = () => { },
+    cbZero = () => { },
+}) => {
+    const { min, value, max, higher } = timeObject[part];
+    if (value > min) {
+        timeObject[part].value -= 1;
+        cbSuccess();
+        return;
     }
-    if (seconds !== undefined) {
-        acc += seconds * MS100_IN_SECONDS;
+    if (higher === undefined) {
+        cbZero();
+        return;
     }
-    if (ms100 !== undefined) {
-        acc += ms100;
-    }
-    return acc;
+    minusTime({
+        timeObject,
+        part: higher,
+        cbSuccess() {
+            timeObject[part].value = max;
+        },
+        cbZero() {
+            cbZero();
+        },
+    });
 };
 
-const changeFullMs100 = () => {
-
+const timeObjectToDom = ({ timeObject, dom }) => {
+    dom.tenthsOfSecond.value = timeObject.tenthsOfSecond.value;
+    dom.seconds.value = timeObject.seconds.value;
+    dom.minutes.value = timeObject.minutes.value;
 };
 
-const changeCounter24 = () => {
-
+const setTimeNewValue = ({ timeObject, part, value }) => {
+    timeObject[part].value = value;
 };
-
-
 
 const timeTicker = new TimeTicker({
     delayMs: 100,
     callback() {
-        if (fullMs100 <= 0) {
-            stopTimer();
-            return;
-        }
-
-        fullMs100 -= 1;
-        const timeObject = getTimeObject(fullMs100);
-        dom.minutes.value = timeObject.minutes;
-        dom.seconds.value = timeObject.seconds;
-        dom.ms100.value = timeObject.ms100;
-        if (timeObject.ms100 >= 9) {
-            counter24 = getCorrectCounter24(counter24 - 1, fullMs100);
-            dom.counter24.value = counter24;
-        }
-
-        if (fullMs100 <= 0) {
-            stopTimer();
-        }
+        minusTime({
+            timeObject,
+            part: 'tenthsOfSecond',
+            cbSuccess() {
+                timeObjectToDom({ timeObject, dom });
+            },
+            cbZero() {
+                stopTimer();
+            },
+        });
     },
 });
 
-// saveTimeFromDomToObject({ timeObject, dom });
-// timeToDom({ timeObj, dom });
-
-
-
 // listeners
 dom.minutes.addEventListener('input', debounce((event) => {
-
+    setTimeNewValue({ timeObject, part: 'minutes', value: event.target.value });
+    timeObjectToDom({ timeObject, dom });
 }, listenersDelay));
 dom.seconds.addEventListener('input', debounce((event) => {
-
+    setTimeNewValue({ timeObject, part: 'seconds', value: event.target.value });
+    timeObjectToDom({ timeObject, dom });
 }, listenersDelay));
-dom.ms100.addEventListener('input', debounce((event) => {
-
+dom.tenthsOfSecond.addEventListener('input', debounce((event) => {
+    setTimeNewValue({ timeObject, part: 'tenthsOfSecond', value: event.target.value });
+    timeObjectToDom({ timeObject, dom });
 }, listenersDelay));
 dom.counter24.addEventListener('input', debounce((event) => {
 
@@ -252,7 +183,6 @@ dom.mirror.addEventListener('input', debounce((event) => {
 // buttons add score
 dom.buttons.forEach((button) => {
     const { add, targetName } = button.dataset;
-    // const score = document.querySelector(`[data-name="${targetName}"]`);
     const score = dom[targetName];
 
     button.addEventListener('click', () => {
@@ -261,35 +191,19 @@ dom.buttons.forEach((button) => {
         webSocket.sendJSON({ name: targetName, value });
     });
 });
-// sets
-// dom.sets.forEach((setValue) => {
-//     const { set, targetName } = setValue;
-//     const targetElement = dom[targetName];
-//     if (targetName === 'counter24') {
-//         setValue.addEventListener('click', () => {
-//             targetElement.value = set;
-//         });
-//     }
-// });
 
 // set number
 dom.set5.addEventListener('click', () => {
-    const newFullMs100 = getFullMs100FromTime({ minutes: 5 });
-    console.log('newFullMs100', newFullMs100);
-    // dom.minutes.value = 5;
-    // dom.seconds.value = 0;
-    // dom.ms100.value = 0;
-    // saveTimeFromDomToObject({ timeObject, dom });
-    // sendTime({ timeObject, webSocket });
+    setTimeNewValue({ timeObject, part: 'tenthsOfSecond', value: 0 });
+    setTimeNewValue({ timeObject, part: 'seconds', value: 0 });
+    setTimeNewValue({ timeObject, part: 'minutes', value: 5 });
+    timeObjectToDom({ timeObject, dom });
 });
 dom.set10.addEventListener('click', () => {
-    const newFullMs100 = getFullMs100FromTime({ minutes: 10 });
-    console.log('newFullMs100', newFullMs100);
-    // dom.minutes.value = 10;
-    // dom.seconds.value = 0;
-    // dom.ms100.value = 0;
-    // saveTimeFromDomToObject({ timeObject, dom });
-    // sendTime({ timeObject, webSocket });
+    setTimeNewValue({ timeObject, part: 'tenthsOfSecond', value: 0 });
+    setTimeNewValue({ timeObject, part: 'seconds', value: 0 });
+    setTimeNewValue({ timeObject, part: 'minutes', value: 10 });
+    timeObjectToDom({ timeObject, dom });
 });
 dom.set14.addEventListener('click', () => {
     // saveCounter24({ timeObject, dom, newValue: 14 });
