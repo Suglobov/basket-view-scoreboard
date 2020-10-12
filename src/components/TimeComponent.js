f55227ae07acda19658e180ab4d57fe10241b7c1
suglobov <suglobov@mail.ru>
2020-10-12 15:35:25 +0700

    📦refactor: Создал класс для компонентов времени. Начал менять способ отправки данных.

---
+++ src/components/TimeComponent.js
@@ -0,0 +1,26 @@
+class TimeComponent {
+    constructor({
+        value = 0,
+        min = 0,
+        max = 0,
+        higher,
+    }) {
+        this.value = value;
+        this.min = min;
+        this.max = max;
+        this.higher = higher;
+    }
+
+    setValue(value) {
+        if (value >= this.min && value <= this.max) { 
+            this.value = value;
+        } else if (value < this.min) {
+            this.value = this.min;
+        } else if (value > this.max) {
+            this.value = this.max;
+        }
+        return this;
+    }
+}
+
+export default TimeComponent;
