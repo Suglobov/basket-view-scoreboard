629fce6f86b0630dd094e499907e1b04390a99e5
suglobov <suglobov@mail.ru>
2020-10-16 15:11:22 +0700

    🐛fix: Подправил, чтоб ошибки не падали в консоль.

--- src/components/EventsStorage.js
+++ src/components/EventsStorage.js
@@ -7,22 +7,28 @@ class EventsStorage {
     }
     on(eventName, eventHandler) {
         if (this.events[eventName] === undefined) {
-            console.log('not posible eventName', eventName);
+            console.warn('not posible eventName', eventName);
+            return this;
         }
         this.events[eventName].push(eventHandler);
+        return this;
     }
     off(eventName, eventHandler) {
         if (this.events[eventName] === undefined) {
-            console.log('not posible eventName', eventName);
+            console.warn('not posible eventName', eventName);
+            return this;
         }
         this.events[eventName] = this.events[eventName]
             .filter((handler) => handler !== eventHandler);
+        return this;
     }
     trigger(eventName, ...args) {
         if (this.events[eventName] === undefined) {
-            console.log('not posible eventName', eventName);
+            console.warn('not posible eventName', eventName);
+            return this;
         }
         this.events[eventName].forEach((handler) => handler(...args));
+        return this;
     }
 }
 
