eefa66f789c655f580ecf1b40a8ad207bda38b5a
suglobov <suglobov@mail.ru>
2020-10-28 16:55:37 +0700

    📦refactor: Переписал с более удачным вариантом класса.

--- src/components/CountdownObject.js
+++ src/components/CountdownObject.js
@@ -1,69 +1,71 @@
-import CountdownTimer from './CountdownTimer.js';
-import ValueComponent from './ValueComponent.js';
+import Countdown from './Countdown.js';
 import EventsStorage from './EventsStorage.js';
 
 class CountdownObject {
     constructor() {
-        this.time = new CountdownTimer([
-            new ValueComponent({ value: 0, min: 0, max: 9 }),
-            new ValueComponent({ value: 0, min: 0, max: 59 }),
-            new ValueComponent({ value: 0, min: 0, max: 10 }),
-        ]);
-        this.counter24 = new CountdownTimer([
-            new ValueComponent({ value: 0, min: 0, max: 9 }),
-            new ValueComponent({ value: 0, min: 0, max: 24 }),
-        ]);
+        this.timer = new Countdown({ fullTenthsMax: 6000 });
+        this.counter24 = new Countdown({ fullTenthsMax: 240 });
         this.events = new EventsStorage([
-            'changed',
             'zero',
+            'change',
+            'minusTenths',
+            'changeParts',
         ]);
     }
-    change({ tenthsOfSecond, seconds, minutes, counter24 }) {
-        this.time.change([tenthsOfSecond, seconds, minutes]);
-        this.counter24.change([tenthsOfSecond, counter24]);
-        this.syncTenth().correctCounter24();
-        const maxChangedIndex = Math.max(
-            [tenthsOfSecond, seconds, minutes].reduce((acc, elem, index) => elem === undefined ? acc : index, 0),
-            [tenthsOfSecond, counter24].reduce((acc, elem, index) => elem === undefined ? acc : index, 0),
-        );
-        this.events.trigger('changed', maxChangedIndex);
-        return this;
-    }
-    give() {
-        return {
-            time: this.time.give(),
-            counter24: this.counter24.give(),
-        };
-    }
-    syncTenth() {
-        this.counter24.change([this.time.give()[0].value]);
-        return this;
-    }
-    correctCounter24() {
-        const times = this.time.give();
-        const counter24s = this.counter24.give();
-        if (
-            times[2].value === times[2].min
-            && times[1].value < counter24s[1].value
-        ) {
-            counter24s[1].setValue(times[1].value);
+    _correctCounter24() {
+        if (this.counter24.fullTenths > this.timer.fullTenths) {
+            this.counter24.changeTenths(this.timer.fullTenths);
         }
         return this;
     }
+    _getPrevValues() {
+        return {
+            timer: {
+                fullTenths: this.timer.fullTenths,
+                tenths: this.timer.tenths,
+                seconds: this.timer.seconds,
+                minutes: this.timer.minutes,
+            },
+            counter24: {
+                fullTenths: this.counter24.fullTenths,
+                tenths: this.counter24.tenths,
+                seconds: this.counter24.seconds,
+                minutes: this.counter24.minutes,
+            },
+        };
+    }
+    changeParts({ timer, counter24 }) {
+        const prevValues = this._getPrevValues();
+        if (timer !== undefined) {
+            const { tenths, seconds, minutes } = timer;
+            this.timer.changeParts({ tenths, seconds, minutes });
+        }
+        if (counter24 !== undefined) {
+            const { tenths, seconds, minutes } = counter24;
+            this.counter24.changeParts({ tenths, seconds, minutes });
+        }
+        this._correctCounter24();
+        this.events.trigger('change', prevValues);
+        this.events.trigger('changeParts', prevValues);
+    }
     checkForZero() {
-        const isZero = this.time.isZero();
+        const isZero = this.timer.fullTenths === 0;
         if (isZero) {
             this.events.trigger('zero');
         }
         return this;
     }
-    minus1() {
-        const maxChangedIndex = this.time.minus1();
-        this.counter24.minus1();
-
-        if (maxChangedIndex !== -1) {
-            this.events.trigger('changed', maxChangedIndex);
+    minusTenth() {
+        if (this.timer.fullTenths === 0) {
+            this._correctCounter24();
+            return this;
         }
+        const prevValues = this._getPrevValues();
+        this.timer.changeTenths(this.timer.fullTenths - 1);
+        this.counter24.changeTenths(this.counter24.fullTenths - 1);
+        this._correctCounter24();
+        this.events.trigger('change', prevValues);
+        this.events.trigger('minusTenths', prevValues);
         return this;
     }
 }
