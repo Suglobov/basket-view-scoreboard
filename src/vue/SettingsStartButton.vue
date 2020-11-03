3d121016b2d7ffed77ac7a83c04627fff404440c
suglobov <suglobov@mail.ru>
2020-11-03 19:41:56 +0700

    📦refactor: Вынес кнопку включения выключения таймера в отдельный компонент.

---
+++ src/vue/SettingsStartButton.vue
@@ -0,0 +1,51 @@
+<template>
+    <div v-tooltip="'пробел'">
+        <button
+            v-if="isTimeRunning"
+            :class="$style.stopTimer"
+            @click="$emit('stop-timer')"
+        >
+            Пауза
+        </button>
+        <button
+            v-else
+            :class="$style.startTimer"
+            @click="$emit('start-timer')"
+        >
+            Старт
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isTimeRunning: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    emits: [
+        'stop-timer',
+        'start-timer',
+    ],
+};
+</script>
+
+<style module>
+.startTimer,
+.stopTimer {
+    font-size: 4vw;
+    padding: 2vw;
+}
+
+.startTimer {
+    background-color: #1e87f0;
+    color: #fff;
+}
+
+.stopTimer {
+    background-color: #f0506e;
+    color: #fff;
+}
+</style>
