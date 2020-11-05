7ce1eae2c5931b648943d5a260e9d0a4c3d0dbe7
suglobov <suglobov@mail.ru>
2020-11-05 16:17:16 +0700

    📦refactor:
    Переписал класс так, чтоб выдавал предупреждения, но не останавливал работу.

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,33 +1,34 @@
 export default class {
     constructor({ fullTenthsMax = 0 }) {
-        if (Number.isInteger(fullTenthsMax) === false) {
-            console.error(`${fullTenthsMax} is not integer`);
-            return this;
-        }
+        const fullTenthsMaxInteger = this._getInteger(fullTenthsMax);
 
         this.TENTHS_IN_SECONDS = 10;
         this.TENTHS_IN_MINUTES = 600;
-        this._fullTenthsMax = fullTenthsMax;
+        this._fullTenthsMax = fullTenthsMaxInteger;
         this.fullTenths = 0;
         this.tenths = 0;
         this.seconds = 0;
         this.minutes = 0;
     }
+    _getInteger(value) {
+        if (Number.isInteger(value) === false) {
+            console.warn(`${value} is not integer`);
+            return 0;
+        }
+        return value;
+    }
     valueOf() {
         return this.fullTenths;
     }
     changeTenths(value) {
-        if (Number.isInteger(value) === false) {
-            console.error(`${value} is not integer`);
-            return this;
-        }
+        const valueInteger = this._getInteger(value);
 
         const prevFullTenths = this.fullTenths;
 
-        if (value >= 0 && value <= this._fullTenthsMax) {
-            this.fullTenths = value;
+        if (valueInteger >= 0 && valueInteger <= this._fullTenthsMax) {
+            this.fullTenths = valueInteger;
         } else {
-            this.fullTenths = value < 0 ? 0 : this._fullTenthsMax;
+            this.fullTenths = valueInteger < 0 ? 0 : this._fullTenthsMax;
         }
 
         if ((prevFullTenths - this.fullTenths) === 0) {
