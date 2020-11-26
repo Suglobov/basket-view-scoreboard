b63c5093ebcfde37de943069daeb15a17f490b9f
suglobov <suglobov@mail.ru>
2020-11-26 16:40:38 +0700

    📦refactor: Частично добавил проверку типов.

--- src/components/TimeComponent.js
+++ src/components/TimeComponent.js
@@ -1,9 +1,9 @@
 import LimitInteger from './LimitInteger.js';
+import checkType from './checkType.js';
 
 export default class {
     constructor ({ fullTenthsMax = 0 }) {
-        this.limitInteger = new LimitInteger({ value: 0, min: 0, max: fullTenthsMax });
-        this.fullTenths = this.limitInteger.value;
+        this.limitInteger = new LimitInteger({ max: fullTenthsMax });
 
         this.rules = [
             { name: 'tenths', divider: 1, remainder: 10 },
@@ -12,55 +12,67 @@ export default class {
         ];
         this.names = Object.values(this.rules).map((elem) => elem.name);
         this._saveComponents(this.names.reduce((res, elem) => ({ ...res, [elem]: 0 }), {}));
+
+        Object.freeze(this.rules);
+        Object.freeze(this.names);
     }
 
-    _getComponents (fullValue) {
-        const componentObject = this.rules.reduce((res, { name, divider, remainder }) => {
+    _getPartsFromFullValue (fullValue = 0) {
+        checkType(fullValue, 'integer');
+        const parts = this.rules.reduce((res, { name, divider, remainder }) => {
             res[name] = ~~(fullValue / divider % remainder);
             return res;
         }, {});
-        return componentObject;
+        return parts;
     }
 
-    _getFullValueFromComponents (componentObject) {
+    _getFullValueFromParts (parts = {}) {
         const fullValue = this.rules.reduce((res, { name, divider }) => {
-            const value = (componentObject[name] === undefined)
-                ? this[name] * divider
-                : componentObject[name] * divider;
+            const part = (parts[name] === undefined) ? this[name] : parts[name];
+            checkType(part, 'integer');
+            const value = part * divider;
             return res + value;
         }, 0);
         return fullValue;
     }
 
-    _saveComponents (componentObject) {
-        this.names.forEach((name) => (this[name] = componentObject[name]));
+    _saveComponents (parts = {}) {
+        this.names.forEach((name) => {
+            if (parts[name] === undefined) {
+                return;
+            }
+            checkType(parts[name], 'integer');
+            this[name] = parts[name];
+        });
     }
 
-    changeTenths (value) {
-        const oldValue = this.limitInteger.value;
-        this.limitInteger.changeValue(value);
-        if (this.limitInteger.value !== value) {
-            console.warn('this.limitInteger.value !== value', this.limitInteger.value, value);
-        }
+    getFullTenths () {
+        return this.limitInteger.getValue();
+    }
 
-        if (oldValue === this.limitInteger.value) {
+    getParts () {
+        const out = this.rules.reduce((res, { name }) => {
+            res[name] = this[name];
+            return res;
+        }, {});
+        return out;
+    }
+
+    changeTenths (value = 0) {
+        checkType(value, 'integer');
+        const oldValue = this.limitInteger.getValue();
+        this.limitInteger.setValue(value);
+
+        if (oldValue === value) {
             return;
         }
-        this.fullTenths = this.limitInteger.value;
 
-        const componentObject = this._getComponents(this.fullTenths);
-        this._saveComponents(componentObject);
-        return this;
+        const parts = this._getPartsFromFullValue(value);
+        this._saveComponents(parts);
     }
 
-    changeParts (componentObject) {
-        const fullTenths = this._getFullValueFromComponents(componentObject);
+    changeParts (parts = {}) {
+        const fullTenths = this._getFullValueFromParts(parts);
         this.changeTenths(fullTenths);
-        return this;
-    }
-
-    valueOf () {
-        console.warn('valueOf');
-        return this.fullTenths;
     }
 }
