7e59ae45dc385c10f6b52321ba0eaf348bfe3b5f
suglobov <suglobov@mail.ru>
2020-11-29 21:44:13 +0700

    📦refactor: Удалил директиву tooltip.

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue';
 import SettingsApp from '../vue/SettingsApp.vue';
-import tooltipVueDirective from '../vue/tooltipVueDirective.js';
 
 createApp(SettingsApp)
-    .directive('tooltip', tooltipVueDirective)
     .mount('#app');
