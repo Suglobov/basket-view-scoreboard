c172fd893657ac79cafc372c174dabef0687afa3
Eugeny Suglobov <es@keyba.ru>
2020-12-18 14:25:48 +0700

    Убрал лишнее

--- src/components/TimerManager.js
+++ src/components/TimerManager.js
@@ -1,8 +1,6 @@
 import TimeTicker from '../components/TimeTicker.js';
 import TimeComponent from '../components/TimeComponent.js';
 import EventsStorage from '../components/EventsStorage.js';
-import TimerTenths2 from '../components/TimerTenths2.js';
-import TimerTenthWrapper from '../components/TimerTenthWrapper.js';
 
 
 export default class {
@@ -24,13 +22,6 @@ export default class {
         ]);
 
         this._listenTimeTicker();
-
-        const timerTenths2 = new TimerTenths2({ maxValue: 6000 });
-        console.log('timerTenths2', timerTenths2);
-        globalThis.timerTenths2 = timerTenths2;
-        const timerTenthWrapper = new TimerTenthWrapper({ maxValue: 6000 });
-        console.log('timerTenthWrapper', timerTenthWrapper, timerTenthWrapper.maxValue);
-        globalThis.timerTenthWrapper = timerTenthWrapper;
     }
 
     _listenTimeTicker () {
