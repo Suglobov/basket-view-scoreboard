1050d241af65c6257d07646ec86831f44a20b704
Eugeny Suglobov <es@keyba.ru>
2021-01-23 22:37:14 +0700

    📦refactor:
    Переписал с использованием функции по проверки инстанса,
    и с использованием GoNext.

--- src/components/EventsStorage.js
+++ src/components/EventsStorage.js
@@ -1,99 +1,89 @@
-import { waterFall, deepFreeze } from '../components/helpers.js';
+import { deepFreeze, checkInstanceOf, GoNext } from '../components/helpers.js';
 
-const checkString = (string, cbNext = () => { }) => {
-    if (typeof string !== 'string') {
-        console.warn(new Error(`'${string}' not string`));
-        return;
-    }
 
-    cbNext();
-};
-
-const checkFunction = (func, cbNext = () => {}) => {
-    if (func instanceof Function === false) {
-        console.warn(new Error(`'${func}' not function`));
-        return;
-    }
-
-    cbNext();
-};
-
-const checkDuplicateEventName = (events, eventName, cbNext = () => { }) => {
-    if (events[eventName] !== undefined) {
-        console.warn(new Error(`'${eventName}' eventName duplicate`));
-        return;
-    }
-
-    cbNext();
-};
-
-const checkEventName = (events, eventName, cbNext = () => { }) => {
+const checkEventNamePossible = (events, eventName, cbNext = () => { }) => {
     if (events[eventName] === undefined) {
         console.warn(new Error(`'${eventName}' eventName not possible`));
         return;
     }
-
     cbNext();
 };
-
 const checkDuplicateHandler = (events, eventName, eventHandler, cbNext = () => { }) => {
     if (events[eventName].some((handler) => handler === eventHandler)) {
         console.warn(new Error(`'${eventHandler}' eventHandler duplicate`));
         return;
     }
-
     cbNext();
 };
+const checkDuplicateEventName = (events, eventName, cbNext = () => { }) => {
+    if (events[eventName] !== undefined) {
+        console.warn(new Error(`'${eventName}' eventName duplicate`));
+        return;
+    }
+    cbNext();
+};
+
 
 export default class EventsStorage {
     constructor (possibleEvents = []) {
         const events = Object.create(null);
-        this.possibleEvents = possibleEvents;
+        this.possibleEvents = undefined;
 
         this.on = undefined;
         this.off = undefined;
         this.trigger = undefined;
 
 
-        if (Array.isArray(possibleEvents) === false) {
-            console.warn(new Error('possibleEvents not array'));
-            this.possibleEvents = [];
-        }
-
+        new GoNext(({ toNext, toError }) => {
+            checkInstanceOf({ value: possibleEvents, type: Array, cbOk: toNext, cbFail: toError });
+        })
+            .next(() => (this.possibleEvents = possibleEvents))
+            .error(() => (this.possibleEvents = []));
 
         this.possibleEvents.forEach((eventName) => {
-            waterFall(
-                (cbNext) => checkString(eventName, cbNext),
-                (cbNext) => checkDuplicateEventName(events, eventName, cbNext),
-                () => (events[eventName] = []),
-            );
+            new GoNext(({ toNext, toError }) => {
+                checkInstanceOf({ value: eventName, type: String, cbOk: toNext, cbFail: toError });
+            }).next(({ toNext }) => {
+                checkDuplicateEventName(events, eventName, toNext);
+            }).next(() => {
+                events[eventName] = [];
+            });
         });
 
         this.on = (eventName, eventHandler) => {
-            waterFall(
-                (cbNext) => checkString(eventName, cbNext),
-                (cbNext) => checkEventName(events, eventName, cbNext),
-                (cbNext) => checkFunction(eventHandler, cbNext),
-                (cbNext) => checkDuplicateHandler(events, eventName, eventHandler, cbNext),
-                () => events[eventName].push(eventHandler),
-            );
+            new GoNext(({ toNext }) => {
+                checkInstanceOf({ value: eventName, type: String, cbOk: toNext });
+            }).next(({ toNext }) => {
+                checkInstanceOf({ value: eventHandler, type: Function, cbOk: toNext });
+            }).next(({ toNext }) => {
+                checkEventNamePossible(events, eventName, toNext);
+            }).next(({ toNext }) => {
+                checkDuplicateHandler(events, eventName, eventHandler, toNext);
+            }).next(() => {
+                events[eventName].push(eventHandler);
+            });
         };
 
         this.off = (eventName, eventHandler) => {
-            waterFall(
-                (cbNext) => checkString(eventName, cbNext),
-                (cbNext) => checkEventName(events, eventName, cbNext),
-                (cbNext) => checkFunction(eventHandler, cbNext),
-                () => (events[eventName] = events[eventName].filter((handler) => handler !== eventHandler)),
-            );
+            new GoNext(({ toNext }) => {
+                checkInstanceOf({ value: eventName, type: String, cbOk: toNext });
+            }).next(({ toNext }) => {
+                checkInstanceOf({ value: eventHandler, type: Function, cbOk: toNext });
+            }).next(({ toNext }) => {
+                checkEventNamePossible(events, eventName, toNext);
+            }).next(() => {
+                events[eventName] = events[eventName].filter((handler) => handler !== eventHandler);
+            });
         };
 
         this.trigger = (eventName, ...args) => {
-            waterFall(
-                (cbNext) => checkString(eventName, cbNext),
-                (cbNext) => checkEventName(events, eventName, cbNext),
-                () => (events[eventName].forEach((handler) => handler(...args))),
-            );
+            new GoNext(({ toNext }) => {
+                checkInstanceOf({ value: eventName, type: String, cbOk: toNext });
+            }).next(({ toNext }) => {
+                checkEventNamePossible(events, eventName, toNext);
+            }).next(() => {
+                events[eventName].forEach((handler) => handler(...args));
+            });
         };
 
         deepFreeze(this);
